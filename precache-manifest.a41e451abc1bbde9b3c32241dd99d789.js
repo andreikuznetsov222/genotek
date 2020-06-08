2f005aaf7c04a5a629cb22b24c1c9ebe94ad9f59
andreikuznetsov222 <samurai22220@gmail.com>
2020-06-08 19:30:01 +0300

    Updates

---
+++ precache-manifest.a41e451abc1bbde9b3c32241dd99d789.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b5098d8e13ab8eda59a24a9013179be",
+    "url": "/genotek/index.html"
+  },
+  {
+    "revision": "b7d131c52c6ed63e9fe0",
+    "url": "/genotek/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f1822e118b7798ced6c8",
+    "url": "/genotek/static/js/2.b119aab4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/genotek/static/js/2.b119aab4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d131c52c6ed63e9fe0",
+    "url": "/genotek/static/js/main.5650b572.chunk.js"
+  },
+  {
+    "revision": "388f437f3ddf44bfaabf",
+    "url": "/genotek/static/js/runtime-main.9d258f42.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/genotek/static/media/logo.5d5d9eef.svg"
+  }
+]);
